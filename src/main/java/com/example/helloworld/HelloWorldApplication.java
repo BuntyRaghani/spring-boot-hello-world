58da104b4ebb2af04873fd03b0803d6d145af8b2
akram trimech <96310223+Akram6Trimech9@users.noreply.github.com>
2022-10-25 02:00:44 +0100

    Update HelloWorldApplication.java

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -8,6 +8,7 @@ public class HelloWorldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
+        	app.setBannerMode(Banner.Mode.OFF);
     }
 
 }
