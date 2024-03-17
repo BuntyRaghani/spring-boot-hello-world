7cff9757b90f38a926fd2966e507ff7ea07b186a
kishoraswar22 <136694063+kishoraswar22@users.noreply.github.com>
2024-03-17 11:15:44 +0530

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, World!";
+        return "Hello, World DveOps!";
     }
 }
